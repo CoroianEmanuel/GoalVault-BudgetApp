272b38daa7abae64ce2a98d50e028b0337233874
CoroianEmanuel <151685991+CoroianEmanuel@users.noreply.github.com>
2025-03-22 08:21:56 +0200

    Auth ready without api call (1:52:04).

---
+++ Licenta-GoalVault/src/App.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+import Login from "./pages/Auth/Login"
+import SignUp from "./pages/Auth/SignUp"
+import Home from "./pages/Deashboard/Home"
+import Income from "./pages/Deashboard/Income"
+import Expense from "./pages/Deashboard/Expenses"
+
+const App = () => {
+  return (
+    <div>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Root />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/deashboard" element={<Home />} />
+          <Route path="/income" element={<Income />} />
+          <Route path="/expense" element={<Expense />} />
+        </Routes>
+      </Router>
+    </div>
+  )
+}
+
+export default App
+
+
+const Root = () => {
+  const isAuthentificated = !!localStorage.getItem("token");
+
+  return isAuthentificated ? (
+  <Navigate to="/deashboard" />
+  ) : (
+    <Navigate to="/login" />
+  );
+};
