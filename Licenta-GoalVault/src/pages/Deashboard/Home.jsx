f5a333b5e5dbb04fb693345d123f3ab335e6289e
CoroianEmanuel <151685991+CoroianEmanuel@users.noreply.github.com>
2025-04-02 18:45:39 +0300

    First chart ready (pie chart), missing totalBalance inside the chart

--- Licenta-GoalVault/src/pages/Deashboard/Home.jsx
+++ Licenta-GoalVault/src/pages/Deashboard/Home.jsx
@@ -8,6 +8,8 @@ import InfoCard from "../../components/Cards/InfoCard";
 import { addThousandsSeparator } from "../../utils/helper";
 import { LuHandCoins, LuWalletMinimal } from "react-icons/lu";
 import {IoMdCard} from "react-icons/io";
+import RecentTransactions from "../../components/Dashboard/RecentTransactions";
+import FinanceOverview from "../../components/Dashboard/FinanceOverview";
 
 const Home = () => {
     useUserAuth();
@@ -42,7 +44,7 @@ const Home = () => {
     return (
         <DashboardLayout activeMenu="Dashboard">
             <div className="my-5 mx-auto">
-                <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+                 <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
                     <InfoCard
                         icon={<IoMdCard />}
                         label="Total balance"
@@ -60,6 +62,19 @@ const Home = () => {
                         label="Total Expense"
                         value={addThousandsSeparator(dashboardData?.totalExpense || 0)}
                         color="bg-red-400"
+                    />
+                </div> 
+
+                <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mt-6">
+                    <RecentTransactions
+                        transactions={dashboardData?.recentTransactions}
+                        onSeeMore={() => navigate("/expense")}
+                        />
+
+                    <FinanceOverview 
+                        totalBalance={dashboardData?.totalBalance || 0}
+                        totalIncome={dashboardData?.totalIncome || 0}
+                        totalExpense={dashboardData?.totalExpense || 0}
                     />
                 </div>
             </div>
